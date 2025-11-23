e2ef374c1742ae1a4df0bd77698a0e8b95c5f207
Sankalp-Space <sankalpshrivastav2001@gmail.com>
2025-11-24 00:55:08 +0530

    logo sticker peel added

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,28 +1,34 @@
 "use client";
 
-import React from 'react'
-import HeroContent from '../sub/HeroContent';
+import React from "react";
+import HeroContent from "../sub/HeroContent";
 import StarsCanvas from "@/components/main/starBackground";
-import { motion } from 'framer-motion';
-import { slideInFromTop } from '@/utils/motion';
+import { motion } from "framer-motion";
+import { slideInFromTop } from "@/utils/motion";
+import StickerPeel from '../sub/StickerPeel' ;
+
 
 const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
-      <motion.img
-        src="/sankalp_space.png"
-        alt="Logo"
-        className="absolute top-4 left-4 w-40 z-50 object-contain"
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
+    <div className="relative flex flex-col h-full w-full">
+      
+      <StickerPeel
+        imageSrc='/sankalp_space.png'
+        width={200}
+        rotate={0}
+        peelBackHoverPct={55}
+        peelBackActivePct={40}
+        shadowIntensity={1}
+        lightingIntensity={1}
+        // initialPosition={{ x: -100, y: 100 }}
       />
       {/* <video autoPlay muted loop className='rotated-180 absolute top-[-200px] left-0 z-[-1]  w-full h-full object-cover' >
         <source  src='/blackhole.webm' type='video/webm'/>
       </video> */}
-      <StarsCanvas/>
-      <HeroContent/>
+      <StarsCanvas />
+      <HeroContent />
     </div>
   );
-}
+};
 
 export default Hero;
